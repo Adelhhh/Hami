dec666db9185d7abe169ec45b828ac3cd59108e4
Adel <adelhassanpour.ux@gmail.com>
2024-01-02 16:07:29 +0100

    Initial Commit

---
+++ Hami/HamiApp.swift
@@ -0,0 +1,17 @@
+//
+//  HamiApp.swift
+//  Hami
+//
+//  Created by Adel Hassanpour on 2024-01-02.
+//
+
+import SwiftUI
+
+@main
+struct HamiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
